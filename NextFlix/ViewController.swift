ca4de64d7277db1c4d41457f6baf941bb974ed2c
thiagoleite6 <thiago.0686@gmail.com>
2021-10-12 10:13:52 -0300

    Initial Commit

---
+++ NextFlix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NextFlix
+//
+//  Created by Thiago Leite on 12/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
