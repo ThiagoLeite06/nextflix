a42eab3207ea7a7d0e6ad9000c9ad1c187f5d8cf
Lillian Souza Garcia <>
2021-10-19 11:58:11 -0300

    criacao da tela modal com ligacao da tela series

---
+++ NextFlix/TituloSelecionadoViewController.swift
@@ -0,0 +1,25 @@
+//
+//  TituloSelecionadoViewController.swift
+//  NextFlix
+//
+//  Created by Lilian on 17/10/21.
+//
+
+import UIKit
+
+class TituloSelecionadoViewController: UIViewController {
+
+    @IBOutlet weak var cartazImageView: UIImageView!
+    @IBOutlet weak var tituloSelecionadoLabel: UILabel!
+    @IBOutlet weak var textoSinopseTextView: UITextView!
+    @IBOutlet weak var sinpseLabel: UILabel!
+    @IBOutlet weak var elencoLabel: UILabel!
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
