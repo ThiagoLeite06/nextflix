e513eae1c1f5e93b309b39dfb80a00912b94940f
Guilherme Feitosa <guifeitos@gmail.com>
2021-10-19 23:29:13 -0300

    tratamento tela login e cadastro

--- NextFlix/RegisterScreenViewController.swift
+++ NextFlix/RegisterScreenViewController.swift
@@ -11,6 +11,8 @@ class RegisterScreenViewController: UIViewController {
     
     var register: Register?
     
+    private let regiterViewModel = RegisterViewModel()
+
     @IBOutlet weak var logoUIImage: UIImageView!
     @IBOutlet weak var emailLabel: UILabel!
     @IBOutlet weak var emailTextField: UITextField!
@@ -19,11 +21,13 @@ class RegisterScreenViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        regiterViewModel.delegate = self
         
     }
     
     @IBAction func cadastrarButton(_ sender: Any) {
-        
+        let register = Register(email: emailTextField.text ?? "", senha: senhaTextField.text ?? "")
+        regiterViewModel.registro(register: register)
         
     }
     
@@ -33,4 +37,23 @@ class RegisterScreenViewController: UIViewController {
 //        dismiss(animated: true)
     }
     
+}
+
+extension RegisterScreenViewController: RegisterViewModelDelegate {
+    func registerResult(register: Register) {
+        
+        let alert = UIAlertController(title: "", message: "Cadastro Concluído com Sucesso!!", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "Ok", style: .default) { myAlert in
+            self.limpaTextField()
+        }
+        
+        alert.addAction(okAction)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    private func limpaTextField () {
+        emailTextField.text = ""
+        senhaTextField.text = ""
+    }
 }
--- NextFlix/ViewController.swift
+++ NextFlix/ViewController.swift
@@ -30,13 +30,86 @@ class ViewController: UIViewController {
 
     
     @IBAction func loginButton(_ sender: Any) {
+        
+        if (emailTextField.text == "guilherme@teste.com.br" && senhaTextField.text == "Teste123@") {
+            performSegue(withIdentifier: "", sender: self)
+        }
+        
+        textFieldsVazio()
+        emailTextFieldVazio()
+        senhaTextFieldVazio()
     }
+    
+    private func textFieldsVazio(){
+        if (emailTextField.text!.isEmpty && senhaTextField.text!.isEmpty) {
+            
+            let alert = UIAlertController(title: "Ops", message: "Favor informar login e/ou Senha", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "Ok", style: .default) { myAlert in
+                self.retiraTintaBordaTextFields()
+            }
+            
+            alert.addAction(okAction)
+            self.present(alert, animated: true, completion: nil)
+            pintaTextFields()
+        }
+    }
+    
+    private func emailTextFieldVazio(){
+        if (emailTextField.text!.isEmpty) {
+            
+            let alert = UIAlertController (title: "", message: "Favor informar o Login", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "Ok", style: .default) { myAlert in
+                self.retiraTintaBordaTextFields()
+            }
+            alert.addAction(okAction)
+            self.present(alert, animated: true, completion: nil)
+            pintaTextFieldEmail()
+        }
+    }
+    
+    private func senhaTextFieldVazio() {
+        if (senhaTextField.text!.isEmpty) {
+            
+            let alert = UIAlertController (title: "", message: "Favor informar o Senha", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "Ok", style: .default) { myAlert in
+                self.retiraTintaBordaTextFields()
+                
+            }
+            alert.addAction(okAction)
+            self.present(alert, animated: true, completion: nil)
+            pintaTextFieldSenha()
+        }
+    }
+    
+    private func pintaTextFields() {
+        emailTextField.layer.borderColor = UIColor.red.cgColor
+        emailTextField.layer.borderWidth = 2
+        senhaTextField.layer.borderColor = UIColor.red.cgColor
+        senhaTextField.layer.borderWidth = 2
+    }
+    
+    private func pintaTextFieldEmail() {
+        emailTextField.layer.borderColor = UIColor.red.cgColor
+        emailTextField.layer.borderWidth = 2
+    }
+    
+    private func pintaTextFieldSenha() {
+        senhaTextField.layer.borderColor = UIColor.red.cgColor
+        senhaTextField.layer.borderWidth = 2
+    }
+    
+    private func retiraTintaBordaTextFields() {
+        self.emailTextField.layer.borderColor = UIColor.clear.cgColor
+        self.senhaTextField.layer.borderColor = UIColor.clear.cgColor
+    }
+    
+    
     
     @IBAction func registerButton(_ sender: Any) {
         
-
-//        performSegue(withIdentifier: "RegisterDetail", sender: RegisterScreenViewController())
-
+        
+        //        performSegue(withIdentifier: "RegisterDetail", sender: RegisterScreenViewController())
+        
         func prepare(for segue: UIStoryboardSegue, sender: Any?) {
             if segue.identifier == "RegisterDetail" {
                 if let register = sender as? Register {
