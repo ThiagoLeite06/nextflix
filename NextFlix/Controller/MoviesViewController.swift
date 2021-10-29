383efecd267b6349e74a11fb85d554e32186089b
thiagoleite6 <thiago.0686@gmail.com>
2021-10-29 12:37:06 -0300

    Finalizando collectionView

--- NextFlix/Controller/MoviesViewController.swift
+++ NextFlix/Controller/MoviesViewController.swift
@@ -9,38 +9,42 @@ import UIKit
 
 class MoviesViewController: UIViewController {
     
-    @IBOutlet weak var moviesTableView: UITableView!
     
-   
+    @IBOutlet weak var tableView: UITableView!
+    
     var movies: [Movie] = [
-        
         .init(imageMovie: #imageLiteral(resourceName: "thelordofthering"), title: "The Lord of the Rings - The Return of the King", release: "25 de dezembro de 2003"),
         .init(imageMovie: #imageLiteral(resourceName: "clubedaluta") , title: "Clube da Luta", release: "29 de outubro de 1999"),
         .init(imageMovie: #imageLiteral(resourceName: "devoltaparafuturo") , title: "De Volta para o Futuro", release: "25 de dezembro de 1985"),
         .init(imageMovie: #imageLiteral(resourceName: "matrix") , title: "Matrix", release: "21 de maio de 1999")
-        
     ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        moviesTableView.dataSource = self
-        moviesTableView.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
     
     
 }
-
+extension MoviesViewController: UITableViewDelegate {
+    
+}
 extension MoviesViewController: UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return movies.count
+        return 2
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if let cell = tableView.dequeueReusableCell(withIdentifier: "MovieTableViewCell", for: indexPath) as? MovieTableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "tableCell", for: indexPath) as? MovieTableViewCell {
+            cell.sessionLbl.text = "Mais procurados"
             
-            cell.movies = movies
+            cell.movieCollectionView.delegate = self
+            cell.movieCollectionView.dataSource = self
+     
             return cell
         }
         
@@ -48,8 +52,25 @@ extension MoviesViewController: UITableViewDataSource{
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 250.0
+        return 350.0
     }
 }
 
-extension MoviesViewController: UITableViewDelegate{}
+extension MoviesViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let collection = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionCell", for: indexPath) as! MovieCollectionViewCell
+        collection.setup(with: movies[indexPath.row])
+        return collection
+    }
+    
+    
+}
+
+extension MoviesViewController: UICollectionViewDelegate {
+    
+}
+
--- NextFlix/View/Components/MovieTableViewCell.swift
+++ NextFlix/View/Components/MovieTableViewCell.swift
@@ -9,52 +9,9 @@ import UIKit
 
 class MovieTableViewCell: UITableViewCell {
     
-    @IBOutlet private weak var movieCollectionView: UICollectionView!
-    
-    var movies: [Movie] = [] {
-        didSet{
-            movieCollectionView.delegate = self
-            movieCollectionView.dataSource = self
-            movieCollectionView.reloadData()
-        }
-    }
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-       
-    }
-    
-    override func willMove(toSuperview newSuperview: UIView?) {
-        super.willMove(toSuperview: newSuperview)
-        
- 
-        
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        
-    }
-    
-}
-
-extension MovieTableViewCell: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return movies.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MoviesCollectionViewCell", for: indexPath) as! MovieCollectionViewCell
-        return cell
-    }
+    @IBOutlet weak var movieCollectionView: UICollectionView!
+    @IBOutlet weak var sessionLbl: UILabel!
     
     
-}
-
-extension MovieTableViewCell: UICollectionViewDelegate {
-    func collectionView(collectionView: UICollectionView, layout: UICollectionViewLayout, sizeForItemAt: IndexPath) -> CGSize {
-        return CGSize(width: 250, height: 250)
-    }
 }
 
