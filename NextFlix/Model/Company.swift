236169784cb8c34a40be45eea7bf375a2253457d
thiagoleite6 <thiago.0686@gmail.com>
2021-11-01 20:02:54 -0300

    Ajuste de modells task05

---
+++ NextFlix/Model/Company.swift
@@ -0,0 +1,19 @@
+//
+//  Company.swift
+//  NextFlix
+//
+//  Created by Thiago Leite on 01/11/21.
+//
+
+import Foundation
+
+struct Company {
+    let description: String
+    let headquarters: String
+    let homepage: String
+    let id: Int
+    let logo_path: String
+    let name: String
+    let origin_country: String
+    let parent_company: String
+}
---
+++ NextFlix/Model/Genre.swift
@@ -0,0 +1,13 @@
+//
+//  Genre.swift
+//  NextFlix
+//
+//  Created by Thiago Leite on 01/11/21.
+//
+
+import Foundation
+
+struct Genre {
+    let id: Int
+    let name: String
+}
